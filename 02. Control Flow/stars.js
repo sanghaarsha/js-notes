4fd33e66d51f22e4637231b0c54e01caf3473c77
sanghaarsha <sanghaarsha@gmail.com>
2020-07-18 23:15:47 +0545

    star pattern added

---
+++ 02. Control Flow/stars.js
@@ -0,0 +1,11 @@
+showStars(3);
+
+function showStars(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let pattern = '';
+        for (let j = 1; j <= i; j++) {
+            pattern += '*';
+        }
+        console.log(pattern);
+    }
+}
