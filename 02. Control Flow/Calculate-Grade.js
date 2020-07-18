86880e98615b72b92bd728db7e0d5cc77af69a4f
sanghaarsha <sanghaarsha@gmail.com>
2020-07-18 23:02:55 +0545

    grade calculator

--- 02. Control Flow/Calculate-Grade.js
+++ 02. Control Flow/Calculate-Grade.js
@@ -4,29 +4,29 @@
 // 80-89 : B
 // 90-100: A
 
-const marks = [80, 80, 50, 90, 10];
+const marks = [80, 80, 50, 90, 40];
 
 function calculateGrade(marks) {
+
+    const averageMarks = calculateAverage(marks)
+
+    if (averageMarks <= 59) return ['F', averageMarks];
+    else if (averageMarks <= 69) return ['D', averageMarks];
+    else if (averageMarks <= 79) return ['C', averageMarks];
+    else if (averageMarks <= 89) return ['B', averageMarks];
+    else if (averageMarks <= 100) return ['A', averageMarks];
+    else return 'error';
+}
+
+function calculateAverage(array) {
+
     let totalSum = 0;
     let count = 0;
-    for (index of marks) {
-        totalSum = index + totalSum;
+    for (let index of array) {
+        totalSum += index;
         count++;
     }
-    let averageMarks = totalSum / count;
-
-    if (averageMarks <= 59)
-        return ['F',averageMarks];
-    else if (averageMarks <= 69)
-        return ['D',averageMarks];
-    else if (averageMarks <= 79)
-        return ['C',averageMarks];
-    else if (averageMarks <= 89)
-        return ['B',averageMarks];
-    else if (averageMarks <= 100)
-        return ['A',averageMarks];
-    else
-        return 'error'
+    return totalSum / count; //instead of count array.length can also be used 
 }
 
 console.log(calculateGrade(marks));
