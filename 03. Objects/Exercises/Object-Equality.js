b83a1a72fdf530d38377e64dd240212800e477f1
sanghaarsha <sanghaarsha@gmail.com>
2020-07-23 14:27:40 +0545

    minor edit

--- 03. Objects/Exercises/Object-Equality.js
+++ 03. Objects/Exercises/Object-Equality.js
@@ -18,7 +18,9 @@ console.log(areSame(address1,address3));
 
 
 function areEqual(address1,address2){
-	return address1.street === address2.street && address1.city === address2.city && address1.zipcode === address2.zipcode;
+	return address1.street === address2.street && 
+			address1.city === address2.city && 
+			address1.zipcode === address2.zipcode;
 }
 
 function areSame(address1,address2){
