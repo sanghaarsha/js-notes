3f11234afcdf46b54269a6ea2f834773b9df65be
sanghaarsha <sanghaarsha@gmail.com>
2020-08-07 10:28:55 +0545

    formatted

--- 04. Arrays/Exercises/Array from Range.js
+++ 04. Arrays/Exercises/Array from Range.js
@@ -1,9 +1,8 @@
-const newArray = arrayFromRange(-1,4);
+const newArray = arrayFromRange(-1, 4);
 console.log(newArray);
 
-function arrayFromRange(min,max) {
+function arrayFromRange(min, max) {
     let newArray = [];
-    for(i=min;i<=max;i++)
-    newArray.push(i);
+    for (i = min; i <= max; i++) newArray.push(i);
     return newArray;
 }
