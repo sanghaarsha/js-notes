6a34a79fba1bc54d0ac890c1c70eeb946f0eff5c
sanghaarsha <sanghaarsha@gmail.com>
2020-08-08 11:15:54 +0545

    more info on moving an element of a array

--- 04. Arrays/Exercises/Moving an Element.js
+++ 04. Arrays/Exercises/Moving an Element.js
@@ -1,15 +1,36 @@
-const numbers = [1, 2, 3, 4, 5];
-const output = move(numbers, 0, 2);
+const numArray = [1,2,3,4,5];
+
+const output = move(numArray,1,1);
+
 console.log(output);
 
-function move(array, index, offset) {
-    const position = index + offset;
-    if (position >= array.length || position < 0) {
-        console.error("Invalid Offset");
-        return;
-    }
-    const output = [...array];
-    const removed = output.splice(index, 1)[0];
-    output.splice(position, 0, removed);
-    return output;
+function move(array,index,offset){
+
+	const position = index + offset;
+
+	if(position >= array.length || position < 0){ 
+		console.error('Invalid Offset');
+		return ;
+	}
+
+	// Lets first copy the array so not to modify original array
+	const newArray = [...array]
+
+	// Now , as part of moving an array
+	// First, we need to remove it from the array
+	// Then, place it at the offset postion
+	
+	// Lets use Splice method to remove the array
+	const removedElement = newArray.splice(index,1);
+	// Storing the removed element so that we can add it back later
+
+	// using splice method to add removedElement
+	newArray.splice(position,0,removedElement[0]);
+
+	// desired moving position = index + offset 
+	// 0 = no items to remove again
+	// removedElement[0] = first element of the removedElement array 
+	// added to index + offset position in the array
+
+	return newArray;
 }
